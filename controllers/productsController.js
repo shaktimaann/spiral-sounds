7f57a881da1774b928f0717f59b9130c4f3fc159
shaktimaann <shashankgade8@gmail.com>
2026-02-24 23:17:27 +0530

    retreived products from database, made filter function working, search enabled

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,8 +1,53 @@
-export async function getProducts(req,res){
-    console.log("get products")
+import { getDBConnection } from '../db/db.js'
+
+export async function getGenres(req, res) {
+
+
+  try {
+
+    const db = await getDBConnection()
+    const genreRows = await db.all(`SELECT DISTINCT genre FROM products`)
+    const genre = genreRows.map(genres=>genres.genre)
+    res.json(genre)
+
+ 
+
+  } catch (err) {
+
+    res.status(500).json({error: 'Failed to fetch genres', details: err.message})
+
+  }
 
 }
 
-export async function getGenres(req,res){
-    console.log("get genres")
+export async function getProducts(req,res) {
+
+    let {search}= req.query
+    const {genre} = req.query
+
+    const db = await getDBConnection()
+    let query = `SELECT * FROM products`
+    let params = []
+
+
+    if(genre){
+        query += ` WHERE genre = ?`
+        params.push(genre)
+    }
+    if(search){
+        search = `%${search}%`
+        query += ` WHERE title LIKE ? OR artist LIKE ? OR genre LIKE ?`
+        params.push(search,search,search)
+        
+    }
+    try{
+    const products = await db.all(query,params)
+    res.json(products)
+
+
+  } catch (err) {
+    res.status(500).json({error: 'Failed to fetch genres', details: err.message})
+
+  }
+
 }
